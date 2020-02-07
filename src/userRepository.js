944726a5f9c6f63cccba0250b848e6b2f2f850c7
Jeremy Poulter <Jpoulter@gmail.com>
2020-02-06 23:34:08 -0700

    Define the declared methods in userRepository class

--- src/userRepository.js
+++ src/userRepository.js
@@ -4,40 +4,66 @@ class UserRepository {
     //this.users should be an array[3]
   }
 
-  returnUserData() {
+  returnUserData(usersId) {
  //input = data.userData.id
  //expected output = user data
- //should user object
+ //should be user
+    return this.users.find(user => {
+      return user.id === usersId
+    })
   }
 
   countUsers() {
     //should return the length of this.users
+    return this.users.length
   }
 
   calculateAverageStepGoal() {
     //should return overall average step goal
     //should be a number
     //.reduce
+    return (this.users.reduce((acc, user) => {
+      acc += user.dailyStepGoal
+      return acc;
+    }, 0) / this.users.length);
   }
 
   calculateAverageStep() {
     //should return overall average step taken
     //should be a number
+    return (activityData.reduce((acc, data) => {
+      acc += data.numSteps;
+      return acc;
+    }, 0) / activityData.length);
   }
 
   calculateAverageSleepQuality() {
     //should return overall average sleepQuality
     //should be a number
+    console.log(sleepData.length);
+    return (sleepData.reduce((acc, data) => {
+      acc += data.sleepQuality;
+      console.log(acc);
+      return acc;
+    }, 0) / sleepData.length);
   }
 
   calculateAverageNumStairsClimbed() {
     //should return overall average stairs climbed
     //should be a number
+    return (activityData.reduce((acc, data) => {
+      acc += data.flightsOfStairs;
+      return acc;
+    }, 0) / activityData.length);
   }
 
   calculateAverageActiveMinutes() {
     //should return overall average minutes active
     //should be a number
+    return (activityData.reduce((acc, data) => {
+      acc += data.minutesActive;
+      return acc;
+    }, 0) / activityData.length);
   }
 }
 
