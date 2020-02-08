156b38a2248c921786801ec2c791cc31769dd5c0
Melissa Steele <melissa.steele09@gmail.com>
2020-02-08 11:35:51 -0700

    Edit user tests

--- test/user-test.js
+++ test/user-test.js
@@ -33,46 +33,43 @@ describe('User', function() {
 
 
   it('should be a function', function() {
-    expect(UserRepository).to.be.a('function');
+    expect(User).to.be.a('function');
   })
 
   it('should be an instance a User', function() {
-    expect(user).to.be.an.instanceof(User);
+    expect(user1).to.be.an.instanceof(User);
   });
 
   it('should have a property that contains user ID', function() {
-    expect(user.id).to.deep.equal(1);
+    expect(user1.id).to.deep.equal(1);
   })
 
   it('should have a property that contains user name' , function() {
-    expect(user.name).to.deep.equal('Luisa Hane');
+    expect(user1.name).to.deep.equal('Luisa Hane');
   })
 
   it('should have a property that contains user address', function() {
-    expect(user.address).to.deep.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697");
+    expect(user1.address).to.deep.equal("15195 Nakia Tunnel, Erdmanport VA 19901-1697");
   })
 
   it('should have a property that contains user email', function() {
-    expect(user.email).to.deep.equal("Diana.Hayes1@hotmail.com");
+    expect(user1.email).to.deep.equal("Diana.Hayes1@hotmail.com");
   })
 
   it('should have a property that contains users average stride length', function() {
-    expect(user.strideLength).to.deep.equal(4.3);
+    expect(user1.strideLength).to.deep.equal(4.3);
   })
 
   it('should have a property that contains users daily step goal', function() {
-    expect(user.dailyStepGoal).to.deep.equal(10000);
+    expect(user1.dailyStepGoal).to.deep.equal(10000);
   })
 
   it('should have a property that contains users friends', function() {
-    expect(user.friends).to.deep.equal([16, 4, 8]);
+    expect(user1.friends).to.deep.equal([2, 3, 4]);
   })
 
   it('should be able to return users first name', function() {
-    expect(user.returnFirstName()).to.deep.equal("Luisa");
+    expect(user1.returnFirstName()).to.deep.equal("Luisa");
   })
 
-  it('should evaluate if step goal has been met', function() {
-    expect(user.evaluateIfStepGoalMet()).to.deep.equal(false);
-  })
 })
