b0c90c6956e00ac6e20bde84daaf6e9e5e96868f
Jeremy Poulter <Jpoulter@gmail.com>
2020-02-08 21:16:10 -0700

    create 2nd half of sleep methods

--- src/sleep.js
+++ src/sleep.js
@@ -1,40 +1,77 @@
 class Sleep {
   constructor(sleepData) {
-    this.userID = sleepData.userId,
-    this.date = sleepData.date,
-    this.hoursSlept = sleepData.hoursSlept,
-    this.sleepQuality = sleepData.sleepQuality
+    this.sleepData = sleepData;
   }
 
-  calculateSleepAverage() {
-    
+  calculateAverageHoursSlept(usersId) {
+    let filteredInfo = this.sleepData.filter(user => user.userID === usersId);
+    return Number((filteredInfo.reduce((acc, user) => {
+      acc += user.hoursSlept;
+      return acc;
+    }, 0) / filteredInfo.length).toFixed(2));
   }
 
-  returnHoursSlept() {
-
+  calculateUsersAverageSleepQuality(usersId) {
+    let filteredInfo = this.sleepData.filter(user => user.userID === usersId);
+    return Number((filteredInfo.reduce((acc, user) => {
+      acc += user.sleepQuality;
+      return acc;
+    }, 0) / filteredInfo.length).toFixed(2));
   }
 
-  returnSleepQuality() {
-
+  returnUsersHoursSleptOnDay(usersId, day) {
+    let filteredUser = this.sleepData.filter(user => user.userID === usersId);
+    let filteredDay = filteredUser.find(user => user.date === day);
+    return filteredDay.hoursSlept;
   }
 
-  returnWeeklyHoursSlept() {
-
+  returnSleepQuality(usersId, day) {
+    let filteredUser = this.sleepData.filter(user => user.userID === usersId);
+    let filteredDay = filteredUser.find(user => user.date === day);
+    return filteredDay.sleepQuality;
   }
 
-  returnWeeklySleepQuality() {
-
+  returnUsersHoursSleptForWeek(usersId, week) {
+    let filteredUser = this.sleepData.filter(user => user.userID === usersId);
+    let weeksInfo = [];
+    week.forEach(day => {
+      weeksInfo.push(filteredUser.find(filtered => filtered.date === day).hoursSlept);
+    });
+    return weeksInfo;
   }
 
-  findQualitySleepers() {
 
+  returnUsersSleepQualityForWeek(usersId, week) {
+    let filteredUser = this.sleepData.filter(user => user.userID === usersId);
+    let weeksInfo = [];
+    week.forEach(day => {
+      weeksInfo.push(filteredUser.find(filtered => filtered.date === day).sleepQuality);
+    });
+    return weeksInfo;
   }
 
-  findHighestSleepers() {
-
+  findAllQualitySleepersForWeek(week) {
+    // let test = [];
+    // let testtwo = this.sleepData;
+    // let filteredDays = week.forEach(day => {
+    //   return testtwo.filter(data => testtwo.date === day)
+    // });
+    // console.log(filteredDays);
+    // let newarr = test.concat(filteredDays);
+    // console.log(newarr);
+    // let test = new Set(this.sleepData.userID);
+    // console.log(test);
   }
 
-  calculateWeeklyQualitySleepChange() {
+  findHighestSleepersOnDay(day) {
+    let filteredDay = this.sleepData.filter(user => user.date === day);
+    let highestHours = filteredDay.reduce((acc, user) => {
+      return (acc.hoursSlept > user.hoursSlept) ? acc : user;
+    }, {}).hoursSlept;
+    return filteredDay.filter(user => user.hoursSlept === highestHours);
+  }
+
+  calculateWeeklyQualitySleepChange(week1, week2) {
 
   }
 }
